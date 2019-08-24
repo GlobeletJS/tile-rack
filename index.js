bb6e8bdfd6063ea4cfb99a840b4dcfa6982e8904
Jeshurun Hembd <jjhembd@gmail.com>
2019-08-24 10:42:02 -0500

    Move index.js to src directory

--- index.js
+++
@@ -1,2 +0,0 @@
-export { initCache } from "./src/cache.js";
-export { cacheTileKiln } from "./src/wrapper.js";
---
+++ src/index.js
@@ -0,0 +1,2 @@
+export { initCache } from "./cache.js";
+export { cacheTileKiln } from "./wrapper.js";
