2ecacefdff7bbad684f8d002722c45e73640ec54
Jeshurun Hembd <jjhembd@gmail.com>
2019-09-23 16:10:52 -0500

    Generalize cache.js to higher-dimension tilesets (e.g., time-varying)

--- README.md
+++ README.md
@@ -23,21 +23,22 @@ initCache requires two parameters:
 The supplied tileFactory function must have a method with the following
 signature:
 ```javascript
-newTile = tileFactory.create(z, x, y)
+newTile = tileFactory.create(...coords)
 ```
-which will return a new tile object at the specified z, x, y indices. The
-object must be returned synchronously, even though the actual image data
-may not yet be ready.
+which will return a new tile object at the specified integer coordinates. The
+object must be returned synchronously, even though the actual image data may
+not yet be ready. The first 3 elements of coords MUST be z, x, y.
 
 The returned tile object must have the following properties:
 - .rendered: A (Boolean) flag indicating whether the tile is ready to use
 - .cancel(): A method to cancel any ongoing data loads or rendering tasks
 
-The tileFactory function may optionally include a method to redraw tiles:
-```javascript
-tileFactory.redraw(tile);
-```
-If supplied, this method will be called when `tile && !tile.rendered`.
+### Optional tileFactory methods
+- `tileFactory.getID(coords)`: A method to generate a unique ID from a given 
+  coordinates array. Note that this ID is needed as the argument to the 
+  getPriority method. If a .getID method is not present, default IDs will be 
+  generated as follows: `id = coords.join("/")`
+- `tileFactory.redraw(tile)`: A method called when `tile && !tile.rendered`.
 
 ## API
 Initialization returns an object with the following methods:
@@ -45,17 +46,15 @@ Initialization returns an object with the following methods:
   a requested tile, and returns either the tile, or a parent tile. Tiles are
   returned in a "box" object with sx, sy, sw properties, indicating the
   cropping and scaling required to match a parent tile to the requested
-  coordinates. The tile itself can be accessed via the `box.tile` property.
+  coordinates. The tile itself can be accessed via the `box.tile` property
 - `process(func)`: Calls `func(tile)` for every tile in the cache
 - `prune(metric, threshold)`: Calls `metric` method on every tile in the cache,
   and writes the returned metric value to a `tile.priority` property. Tiles 
   where `metric(tile.z, tile.x, tile.y) > threshold` are deleted from the
-  cache. Return value is the number of tiles in the cache.
-- `getPriority(id)`: Returns `tile.priority` for the tile with the supplied
-  `id`, where `id` is constructed from the indices of the tile:
-  ```javascript
-  id = z + "/" + x + "/" + y`
-  ```
+  cache. Return value is the number of tiles in the cache
+- `trim(metric, threshold)`: Same as `prune` except the signature of `metric`
+  is the more general `metric(tile)`
+- `getPriority(id)`: Returns `tile.priority` for the tile with the supplied id
 
 ## Use with tile-kiln
 If [tile-kiln](https://github.com/jjhembd/tilekiln) is your preferred tile
--- src/cache.js
+++ src/cache.js
@@ -1,22 +1,24 @@
 export function initCache(size, tileFactory) {
   // Initialize the tiles object
   const tiles = {};
+  const getID = tileFactory.getID || ( zxy => zxy.join("/") );
 
   // Return methods for accessing and updating the tiles
   return {
-    retrieve: (zxy) => getTileOrParent(zxy[0], zxy[1], zxy[2], 0, 0, size),
-    process: (func) => Object.values(tiles).forEach( tile => func(tile) ),
+    retrieve: (zxy) => getTileOrParent(zxy, 0, 0, size),
+    process,
     prune,
+    trim,
     getPriority: (id) => (tiles[id]) ? tiles[id].priority : undefined,
   };
 
   function getTileOrParent(
-      z, x, y,     // Coordinates of the requested tile
-      sx, sy, sw   // Cropping parameters--which part of the tile to use
+      zxy,        // Coordinates of the requested tile (could be more than 3D)
+      sx, sy, sw  // Cropping parameters--which part of the tile to use
       ) {
 
     // Retrieve the specified tile from the tiles object, add cropping info
-    let id = z + "/" + x + "/" + y;
+    let id = getID(zxy);
     let tile = tiles[id];
     let tilebox = { tile, sx, sy, sw };
 
@@ -24,23 +26,32 @@ export function initCache(size, tileFactory) {
     if (tile && tile.rendered) return tilebox;
 
     // Looks like the tile wasn't ready. Try using the parent tile
-    if (z > 0 && sw > 1) { // Don't look too far back
-      // Get coordinates and cropping parameters of the parent
+    if (zxy[0] > 0 && sw > 1) { // Don't look too far back
+      let [z, x, y] = zxy;
+
+      // Get coordinates of the parent tile
       let pz = z - 1;
       let px = Math.floor(x / 2);
       let py = Math.floor(y / 2);
+      // Copy any additional coordinates beyond the first 3
+      let pzxy = [pz, px, py, ...zxy.slice(3)];
+
+      // Compute cropping parameters for the parent
       let psx = sx / 2 + (x / 2 - px) * size;
       let psy = sy / 2 + (y / 2 - py) * size;
       let psw = sw / 2;
 
-      tilebox = getTileOrParent(pz, px, py, psx, psy, psw); // recursive call!
+      // Get the parent tile. Recursive!
+      tilebox = getTileOrParent(pzxy, psx, psy, psw);
     }
 
     // If the requested tile didn't exist, we need to order it from the factory
     // NOTE: orders are placed AFTER the recursive call for the parent tile,
     // so missing parents will be ordered first
-    if (!tile) { 
-      let newTile = tileFactory.create(z, x, y);
+    if (!tile) {
+      // For backwards compatibility, assume tileFactory.create still takes
+      // a list of coordinates as arguments
+      let newTile = tileFactory.create(...zxy);
       if (newTile) tiles[id] = newTile;
     } else if (tileFactory.redraw) {
       // Tile exists but isn't ready. Make sure it is rendering
@@ -52,18 +63,33 @@ export function initCache(size, tileFactory) {
       : undefined;
   }
 
+  function process(func) {
+    Object.values(tiles).forEach( tile => func(tile) );
+  }
+
   function prune(metric, threshold) {
-    // Remove tiles far from current view (as measured by metric)
+    // Update tile priorities using the supplied metric. ASSUMES 3 args!
+    process(tile => { tile.priority = metric(tile.z, tile.x, tile.y); });
+    // Remove tiles where priority is above the threshold
+    return drop(threshold);
+  }
+
+  function trim(metric, threshold) {
+    // Update tile priorities using the supplied metric
+    process(tile => { tile.priority = metric(tile); });
+    // Remove tiles where priority is above the threshold
+    return drop(threshold);
+  }
+
+  function drop(threshold) {
     var numTiles = 0;
-    for ( let id in tiles ) {
-      let tile = tiles[id];
-      tile.priority = metric(tile.z, tile.x, tile.y);
-      if (tile.priority > threshold) {
-        tile.cancel();  // Cancels ongoing data loads or rendering tasks
+    for (let id in tiles) {
+      if (tiles[id].priority > threshold) {
+        tiles[id].cancel();
         delete tiles[id];
-        continue;
+      } else {
+        numTiles ++;
       }
-      numTiles ++;
     }
     return numTiles;
   }
